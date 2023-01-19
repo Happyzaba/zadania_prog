635ccc368a7c0ac38bb1c5d903519d4dab6a15aa
Happyzaba <112874836+Happyzaba@users.noreply.github.com>
2023-01-19 18:55:45 +0100

    Add files via upload

---
+++ Zadania_/Zadania_/AS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotOfTasks
+{
+    class AS
+    {
+        public static void LiczGloski() {
+            int sa = 0;
+            int sp = 0;
+            string samogloski = "aeiou";
+            string spolgloski = "bcdfghjklmnpqrstvwxyz";
+            string zdanie = Console.ReadLine();
+            foreach (char ch in zdanie)
+            {
+                if (samogloski.Contains(ch))
+                {
+                    sa += 1;
+                }
+                else if (spolgloski.Contains(ch)) {
+                    sp += 1;
+                }
+            }
+            Console.WriteLine("samogłoski: " + sa + " spółgłoski: " + sp);
+        }
+    }
+}
